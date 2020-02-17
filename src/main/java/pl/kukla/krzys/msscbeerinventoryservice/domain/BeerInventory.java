b89d85364942eff59a6b2ff2a99e176e6e5b3275
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-02-17 22:21:35 +0100

    162 localmysql profile for mysql configuration, closes #4

--- src/main/java/pl/kukla/krzys/msscbeerinventoryservice/domain/BeerInventory.java
+++ src/main/java/pl/kukla/krzys/msscbeerinventoryservice/domain/BeerInventory.java
@@ -4,7 +4,9 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import java.sql.Timestamp;
 import java.util.UUID;
@@ -28,6 +30,8 @@ public class BeerInventory extends BaseEntity{
     }
 
     //beerId is unique in our control
+    @Type(type="org.hibernate.type.UUIDCharType")
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     private UUID beerId;
 
     //upc should be unique, but is not within our control
