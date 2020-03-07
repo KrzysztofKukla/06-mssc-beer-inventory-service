8f99181c738c1bd2ab39ecd2f0e35f52475ce332
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-03-07 15:54:20 +0100

    230 allocationService, closes #8

---
+++ src/main/java/pl/kukla/krzys/msscbeerinventoryservice/service/AllocationService.java
@@ -0,0 +1,12 @@
+package pl.kukla.krzys.msscbeerinventoryservice.service;
+
+import pl.kukla.krzys.brewery.model.BeerOrderDto;
+
+/**
+ * @author Krzysztof Kukla
+ */
+public interface AllocationService {
+
+    Boolean allocateOrder(BeerOrderDto beerOrderDto);
+
+}
