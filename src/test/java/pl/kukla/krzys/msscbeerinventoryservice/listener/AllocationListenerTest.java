67a2d94d05c4eace2afcfc21fbe7403600723371
Krzysztof Kukla <krzys.kukla@gmail.com>
2020-10-19 21:56:20 +0200

    fake test

---
+++ src/test/java/pl/kukla/krzys/msscbeerinventoryservice/listener/AllocationListenerTest.java
@@ -0,0 +1,10 @@
+package pl.kukla.krzys.msscbeerinventoryservice.listener;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Krzysztof Kukla
+ */
+class AllocationListenerTest {
+
+}
